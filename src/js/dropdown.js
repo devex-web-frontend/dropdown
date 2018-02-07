2b09a6ff2eaf61b2c2803c9113e72d8350dba387
Nikolay Lukinyh <jobnikalun@gmail.com>
2018-02-07 12:40:23 +0300

    Selectbox optimization: dropdown create click on label, destroy dropdown

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -150,8 +150,7 @@ var DropDown = (function(DX) {
 	}
 
 	function createElements(control, config) {
-		var body = document.body,
-			block,
+		var block,
 			list;
 
 		block = DX.Dom.createElement('div', {
@@ -164,8 +163,6 @@ var DropDown = (function(DX) {
 		}
 
 		list = DX.$$('.' + CN_LIST, block);
-
-		body.appendChild(block);
 
 		return {
 			block: block,
@@ -218,16 +215,8 @@ var DropDown = (function(DX) {
 			selectedIndex = 0;
 			hoveredIndex = null;
 			isShownOnce = false;
-
-			initListeners();
-
-			DX.Event.trigger(control, DropDown.E_CREATED, {
-				detail: {
-					block: elements.block,
-					eventTarget: elements.block
-				}
-			});
 		}
+
 		function initListeners() {
 			var block = getEventTarget();
 			block.addEventListener(DropDown.E_HIDE, hide);
@@ -285,7 +274,19 @@ var DropDown = (function(DX) {
 		 * @event dropdown:shown
 		 */
 		function show() {
-			var block = elements.block;
+			var body = document.body,
+				block = elements.block;
+
+            initListeners();
+
+            DX.Event.trigger(control, DropDown.E_CREATED, {
+				detail: {
+					block: elements.block,
+						eventTarget: elements.block
+				}
+            });
+
+            body.appendChild(block);
 
 			if (!isShownOnce) {
 				isShownOnce = true;
@@ -328,6 +329,10 @@ var DropDown = (function(DX) {
 
 			document.removeEventListener(DX.Event.TOUCH_CLICK, documentClickHandler, true);
 			DX.Event.trigger(block, DropDown.E_HIDDEN);
+
+            setTimeout(function() {
+				destroy();
+			}, 250);
 		}
 
 
